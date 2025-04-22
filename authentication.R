e6c18464a4ad69863241c22a8ec49319865600c8
srishti kainth <143260221+srishhk@users.noreply.github.com>
2025-04-22 17:34:05 +0530

    Update authentication.R

--- authentication.R
+++ authentication.R
@@ -7,10 +7,10 @@ library(rtweet)
 # Create Twitter token using OAuth 1.0a method
 token <- rtweet::create_token(
   app = "SrishtiKainth", 
-  consumer_key = "km39WzDubVP1ISYdB0jeqXutI",
-  consumer_secret = "gNVwALjibc7aD9bnHwEcyH2ejYLunVV4IqopxYViE2o4t1vbZv",
-  access_token = "1809615184484528132-6Lt70FHM32TL6idjiVWcAzuqGuzgUa",
-  access_secret = "K4rT7o1YBJDCkcXki5hX19s20RoWyjuZq3apjtgAvT7dQ"
+  consumer_key = "add your consumer key here",
+  consumer_secret = "add your consumer secret here",
+  access_token = "add here your access token here",
+  access_secret = "add your access secret here"
 )
 
 # ✅ Test by fetching latest tweets for a keyword
