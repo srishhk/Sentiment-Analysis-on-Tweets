dccc5bb98935de1f49850a72c76cdd600217eea9
srishti kainth <143260221+srishhk@users.noreply.github.com>
2025-04-22 17:21:11 +0530

    Update authentication.R

--- authentication.R
+++ authentication.R
@@ -6,7 +6,7 @@ library(rtweet)
 
 # Create Twitter token using OAuth 1.0a method
 token <- rtweet::create_token(
-  app = "rakshasinha", 
+  app = "SrishtiKainth", 
   consumer_key = "km39WzDubVP1ISYdB0jeqXutI",
   consumer_secret = "gNVwALjibc7aD9bnHwEcyH2ejYLunVV4IqopxYViE2o4t1vbZv",
   access_token = "1809615184484528132-6Lt70FHM32TL6idjiVWcAzuqGuzgUa",
